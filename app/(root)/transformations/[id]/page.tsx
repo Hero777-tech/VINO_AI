4d9d4bd20041f3bafe9175188222378e89dd95d4
Aditya Nath <pro.codenow@gmail.com>
2024-03-03 19:56:10 +0530

    route-page auth__users

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TransformationsPage = ({ params: {id} }) => { //pending part.
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
+
+//We will be using dynamic routes as we need page.com/transformaers/[id]
+//support link - https://nextjs.org/docs/pages/building-your-application/routing/dynamic-routes
