875b9b39e5213677b033e191949280315260b1d7
Aditya Nath <68598911+Hero777-tech@users.noreply.github.com>
2024-04-03 14:02:43 +0530

    recover middleware

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ export default authMiddleware({
   // no authentication information
   //ignoredRoutes: ['/no-auth-in-this-route'],
   //publicRoutes:['/api/webhooks/clerk'] // /api/webhooks/clerk updated frin api/we....
-  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe' , '/home']
 });
  
 export const config = {
